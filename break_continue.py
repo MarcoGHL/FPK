12f0bfeb2ef8c816e1d7f1cb56ad49b32402f8e3
MarcoGHL <79898179+MarcoGHL@users.noreply.github.com>
2023-04-05 10:39:21 +0200

    added session 3

---
+++ break_continue.py
@@ -0,0 +1,5 @@
+for zahl in range(10):
+  print(zahl)
+  if zahl == 5:
+    print("Zahl gefunden!")
+print("Fertig!")
---
+++ except.py
@@ -0,0 +1,40 @@
+y = 0
+
+try:
+  print(x)
+except:
+  print("Something else went wrong")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""""
+except NameError:
+  print("Variable x is not defined")
+except ValueError:
+    print("Keine Zahl eingegeben!")
+"""
